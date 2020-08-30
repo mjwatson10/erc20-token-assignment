559b8812383ff497893b6a71473b16049ca0f0e4
Mark Watson <64367041+mjwatson10@users.noreply.github.com>
2020-08-30 16:01:17 -0700

    mint tokens deploy

--- migrations/4_erc20_deploy.js
+++ migrations/4_erc20_deploy.js
@@ -2,6 +2,8 @@ const ERC20 = artifacts.require("ERC20");
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(ERC20, "Awesome Sauce", "AWS").then(function(instance){
-    instance.mint(accounts[0], 100);
+    instance.mint(accounts[1], 100).then(function(){
+      console.log(instance.balanceOf(accounts[1]));
+    });
   });
 };
